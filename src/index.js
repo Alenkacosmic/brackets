8b38dd527254d4ae7c6b054a33ba65a82a7ee5c0
Elena Capienko <elena.capienko@gmail.com>
2019-10-13 14:05:48 -0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -3,8 +3,34 @@ module.exports = function check(str, bracketsConfig) {
         return false;
     }
 
-    
-    
+    let expr = '';
 
-  // your solution
+    for (let item = 0; item < str.length; item++) {
+
+        for (let patt = 0; patt < bracketsConfig.length; patt++) { 
+
+            if (bracketsConfig[patt][0] === str[item]) { 
+                if (expr.length && expr[expr.length - 1] === bracketsConfig[patt][0] && bracketsConfig[patt][1] === str[item]) {
+                    expr = expr.slice(0, expr.length - 1);
+
+                } else {
+                    expr += str[item];
+                }
+
+            } else if (bracketsConfig[patt][1] === str[item]) {
+                if (expr[expr.length - 1] === bracketsConfig[patt][0]) {
+                    expr = expr.slice(0, expr.length - 1);
+
+                } else if (expr[expr.length - 1] !== bracketsConfig[patt][0]) {
+                    return false;
+                }
+            }
+        }
+    }
+
+    if (expr.length) {
+        return false;
+    }
+
+return true; // your solution
 }
